817568fa1a640900adfed5b7c22c96c210c72a6d
Abhishek-Satkar <44223866+Abhishek-Satkar@users.noreply.github.com>
2018-10-23 19:50:32 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,16 @@
+n=int(input("Enter number of terms"))
+if n<=0:
+ print("invalid")
+elif num==1:
+ print("0")
+elif n==2:
+ print("0,1")
+else:
+ y=[0,1]
+ i=0,a=0,b=1
+ for i in range(n-2):
+  f=a+b 
+  y.append(f)
+  a=b
+  b=f 
+  print(y)
