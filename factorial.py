57e801bb65e7e32e96515b0190e0230ce0fac5ce
Abhishek-Satkar <44223866+Abhishek-Satkar@users.noreply.github.com>
2018-12-03 19:12:12 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,19 +2,10 @@
 #11810805
 #M 52
 
-n=int(input("Enter number of terms"))
-if n<=0:
- print("invalid")
-elif num==1:
- print("0")
-elif n==2:
- print("0,1")
-else:
- y=[0,1]
- i=0,a=0,b=1
- for i in range(n-2):
-  f=a+b 
-  y.append(f)
-  a=b
-  b=f 
-  print(y)
+a= int(input("Enter a number"))
+i=1
+fact=1
+for j in range(0,a):
+ fact=fact*i
+ i=i+1
+print ("Factorial is:", fact)
